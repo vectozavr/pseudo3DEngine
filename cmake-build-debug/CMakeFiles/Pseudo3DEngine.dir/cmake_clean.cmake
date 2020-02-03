9fae493523a3d79d64b45e107bd786ba45d15e2a
Vectozavr <ivanilin420@gmail.com>
2020-02-03 22:41:52 +0700

    Commit 1

---
+++ cmake-build-debug/CMakeFiles/Pseudo3DEngine.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pseudo3DEngine.dir/main.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/World.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Object2D.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Camera.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Poligon2D.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Circle2D.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Menu.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Level1.cpp.obj"
+  "CMakeFiles/Pseudo3DEngine.dir/Window.cpp.obj"
+  "Pseudo3DEngine.pdb"
+  "Pseudo3DEngine.exe"
+  "Pseudo3DEngine.exe.manifest"
+  "libPseudo3DEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pseudo3DEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
